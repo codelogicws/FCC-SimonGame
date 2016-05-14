fe949c1152e218083b09854ab739ca820d49d1da
codelogicws <russ@codelogic.ws>
2016-05-14 15:33:35 -0700

    Just getting stuff drawen on the canvas.

--- src/Draw.ts
+++ src/Draw.ts
@@ -1,20 +1,30 @@
 /// <reference path='../typings/jquery/jquery.d.ts'/>
 
+
 $(()=>{
 console.log("loaded draw")
 
-$('#myCanvas').html('Hello there');
-var c = <HTMLCanvasElement> $('#myCanvas')[0]
-var ctx = c.getContext("2d")
-ctx.fillStyle = "#FF0000"
-ctx.fillRect(0,0, c.width, c.height)
+  $('#myCanvas').html('Hello there')
+  var c = <HTMLCanvasElement> $('#myCanvas')[0]
+  var ctx = c.getContext("2d")
+  ctx.fillStyle = "#FF0000"
+  ctx.fillRect(0,0, c.width, c.height)
+  ctx.fillStyle = '#000000'
+  createButton()
 
-ctx.moveTo
 
-console.log("loaded draw")
+  console.log("loaded draw")
+
+  function createButton(){
+    ctx.strokeStyle = "#000000"
+    ctx.lineWidth = 3
+    ctx.beginPath()
+    ctx.moveTo(100, 550)
+    ctx.lineTo(200, 550)
+    ctx.lineTo(200, 450)
+    ctx.lineTo(100, 450)
+    ctx.closePath()
+    ctx.stroke()
+  }
+
 })
-
-
-function vmin(){
-  //make normal points act like vmin
-}
