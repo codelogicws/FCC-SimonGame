f8166e1cf016897d9e193d56e8c6b5f42f86ca68
codelogicws <russ@codelogic.ws>
2016-05-14 18:05:50 -0700

    Filling in the simon design more and more.

--- src/Draw.ts
+++ src/Draw.ts
@@ -4,7 +4,7 @@ const PI = Math.PI
 const PI2DIV = Math.PI/2
 const PI2 = Math.PI*2
 const PI1ANDHALF = Math.PI * 1.5
-const BUTTONGAP = 0.03
+const BUTTONGAP = 0.06
 
 $(()=>{
 console.log("loaded draw")
@@ -12,22 +12,36 @@ console.log("loaded draw")
   $('#myCanvas').html('Hello there')
   var c = <HTMLCanvasElement> $('#myCanvas')[0]
   var ctx = c.getContext("2d")
-  ctx.fillStyle = "#FF0000"
-  ctx.fillRect(0,0, c.width, c.height)
-  ctx.fillStyle = '#000000'
-  createButton(PI, PI2DIV, '#770000');
-  createButton(PI2DIV, PI2, '#007700');
-  createButton(PI2, PI1ANDHALF, '#000077');
-  createButton(PI1ANDHALF, PI, '#777700')
+
+  createSimonBase()
+  createCenter()
+  createButton(PI, PI2DIV, '#777700');
+  createButton(PI2DIV, PI2, '#000077');
+  createButton(PI2, PI1ANDHALF, '#770000');
+  createButton(PI1ANDHALF, PI, '#007700')
 
 
   console.log("loaded draw")
 
+  function createCenter(){
+    ctx.fillStyle = '#eee'
+    ctx.beginPath()
+    ctx.arc(500, 500, 200, 0, PI2, false);
+    ctx.fill();
+  }
+
+  function createSimonBase(){
+    ctx.fillStyle = '#111'
+    ctx.beginPath()
+    ctx.arc(500, 500, 500, 0, PI2, false);
+    ctx.fill()
+  }
+
   function createButton(radian1:number, radian2:number, color:string){
     ctx.fillStyle = color
     ctx.beginPath()
-    let innerRadius = 275
-    let outerRadius = 475
+    let innerRadius = 250
+    let outerRadius = 450
     ctx.arc(500, 500, innerRadius, radian1-(BUTTONGAP*(outerRadius/innerRadius)) , radian2 + (BUTTONGAP*(outerRadius/innerRadius)), true)
     ctx.arc(500, 500, outerRadius, radian2 + BUTTONGAP, radian1 - BUTTONGAP, false)
     ctx.fill()
