e95a4994470dc0c3cab66ac46fade66baa4145bf
codelogicws <russ@codelogic.ws>
2016-05-14 21:57:40 -0700

    Still messing with the UI

--- src/Draw.ts
+++ src/Draw.ts
@@ -1,37 +1,41 @@
 /// <reference path='../typings/jquery/jquery.d.ts'/>
+/// <reference path='../typings/browser/definitions/lodash/index.d.ts'/>
 
 const PI = Math.PI
 const PI2DIV = Math.PI/2
 const PI2 = Math.PI*2
 const PI1ANDHALF = Math.PI * 1.5
 const BUTTONGAP = 0.06
-const COLOROFF = 150
-const COLORON = 220
+const COLOROFF = 180
+const COLOROFFDARK = 90
+const COLORON = 230
+const COLORONDARK = 180
 
 $(()=>{
-console.log("loaded draw")
 
-  $('#myCanvas').html('Hello there')
   var c = <HTMLCanvasElement> $('#myCanvas')[0]
   var ctx = c.getContext("2d")
-
-  createSimonBase()
-  addNormalShadow()
-  createCenter()
-  createSmallButton('#ff0', 365, 510)
-  createSmallButton('#f00', 635, 510)
-  createCountDisplay();
-  createButton(PI, PI2DIV, true, true, false)
-  createButton(PI2DIV, PI2, false,false,true)
-  createButton(PI2, PI1ANDHALF, true,false,false)
-  createButton(PI1ANDHALF, PI, false,true,false)
+  drawEverything(0,0,0,1);
 
 
-  ctx.fillStyle = "black"
-  ctx.font = "100px Patua One";
-  ctx.fillText("Simon", 370, 420)
+  function drawEverything(greenButtonOn:boolean, redButtonOn:boolean, blueButtonOn:boolean, yellowButtonOn:boolean){
+    createSimonBase()
+    addNormalShadow()
+    createCenter()
+    createSmallButton('#ff0', 365, 510)
+    createSmallButton('#f00', 635, 510)
+    createCountDisplay();
+    createButton(PI, PI2DIV, true, true, false, yellowButtonOn)
+    createButton(PI2DIV, PI2, false,false,true, blueButtonOn)
+    createButton(PI2, PI1ANDHALF, true,false,false, redButtonOn)
+    createButton(PI1ANDHALF, PI, false,true,false, greenButtonOn)
 
-  console.log("loaded draw")
+
+    ctx.fillStyle = "black"
+    ctx.font = "100px Patua One";
+    ctx.fillText("Simon", 370, 420)
+  }
+
 
   function createCountDisplay(){
     ctx.fillStyle = '#300'
@@ -52,6 +56,10 @@ console.log("loaded draw")
   function createCenter(){
     ctx.beginPath()
     ctx.fillStyle = '#eee'
+    var my_gradient=ctx.createLinearGradient(0,0,800,800);
+    my_gradient.addColorStop(0,'white');
+    my_gradient.addColorStop(1, '#999');
+    ctx.fillStyle=my_gradient;
     ctx.beginPath()
     ctx.arc(500, 500, 200, 0, PI2, false)
     ctx.fill();
@@ -64,14 +72,15 @@ console.log("loaded draw")
     ctx.fill()
   }
 
-  function createButton(radian1:number, radian2:number, redOn:boolean, greenOn:boolean, blueOn:boolean){
-    ctx.fillStyle = getLightColor(redOn, greenOn, blueOn, false);
+  function createButton(radian1:number, radian2:number, redOn:boolean, greenOn:boolean, blueOn:boolean, lightOn:boolean){
+    let curried = _.curry(getLightColor);
+    let big = curried(COLORON, COLOROFF, redOn, greenOn, blueOn);
     ctx.beginPath()
     //gradient
-    // var my_gradient=ctx.createLinearGradient(0,0,800,800);
-    // my_gradient.addColorStop(1,"black");
-    // my_gradient.addColorStop(0,"white");
-    // ctx.fillStyle=my_gradient;
+    var my_gradient=ctx.createLinearGradient(0,0,800,800);
+    my_gradient.addColorStop(0,getLightColor(COLORON, COLOROFF, redOn, greenOn, blueOn, lightOn ));
+    my_gradient.addColorStop(1,getLightColor(COLORONDARK, COLOROFFDARK, redOn, greenOn, blueOn, lightOn ));
+    ctx.fillStyle=my_gradient;
     //-----
     let innerRadius = 250
     let outerRadius = 450
@@ -85,18 +94,18 @@ console.log("loaded draw")
     ctx.shadowBlur = 20
   }
 
-  function getLightColor(redOn:boolean, greenOn:boolean, blueOn:boolean, lightOn:boolean){
+  function getLightColor(onNumber:number, offNumber:number, redOn:boolean, greenOn:boolean, blueOn:boolean, lightOn:boolean){
     return 'rgb(' +
-      ((redOn)? getColor(lightOn) : 0).toString() +
+      ((redOn)? getColor(onNumber, offNumber, lightOn) : 0).toString() +
       ',' +
-      ((greenOn)? getColor(lightOn): 0).toString() +
+      ((greenOn)? getColor(onNumber, offNumber, lightOn): 0).toString() +
       ',' +
-      ((blueOn)? getColor(lightOn): 0).toString() +
+      ((blueOn)? getColor(onNumber, offNumber, lightOn): 0).toString() +
       ')';
   }
 
-  function getColor(lightOn:boolean){
-    return (lightOn)? COLORON : COLOROFF;
+  function getColor(onNumber:number, offNumber:number,lightOn:boolean){
+    return (lightOn)? onNumber : offNumber;
   }
 
 
