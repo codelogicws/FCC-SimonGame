0731e96147fa17acf346124ad1a4ed51b4807e51
codelogicws <russ@codelogic.ws>
2016-05-14 17:20:43 -0700

    There are correctly shaped buttons now.

--- src/Draw.ts
+++ src/Draw.ts
@@ -1,5 +1,10 @@
 /// <reference path='../typings/jquery/jquery.d.ts'/>
 
+const PI = Math.PI
+const PI2DIV = Math.PI/2
+const PI2 = Math.PI*2
+const PI1ANDHALF = Math.PI * 1.5
+const BUTTONGAP = 0.03
 
 $(()=>{
 console.log("loaded draw")
@@ -10,21 +15,22 @@ console.log("loaded draw")
   ctx.fillStyle = "#FF0000"
   ctx.fillRect(0,0, c.width, c.height)
   ctx.fillStyle = '#000000'
-  createButton()
+  createButton(PI, PI2DIV, '#770000');
+  createButton(PI2DIV, PI2, '#007700');
+  createButton(PI2, PI1ANDHALF, '#000077');
+  createButton(PI1ANDHALF, PI, '#777700')
 
 
   console.log("loaded draw")
 
-  function createButton(){
-    ctx.strokeStyle = "#000000"
-    ctx.lineWidth = 3
+  function createButton(radian1:number, radian2:number, color:string){
+    ctx.fillStyle = color
     ctx.beginPath()
-    ctx.moveTo(100, 550)
-    ctx.lineTo(200, 550)
-    ctx.lineTo(200, 450)
-    ctx.lineTo(100, 450)
-    ctx.closePath()
-    ctx.stroke()
+    let innerRadius = 275
+    let outerRadius = 475
+    ctx.arc(500, 500, innerRadius, radian1-(BUTTONGAP*(outerRadius/innerRadius)) , radian2 + (BUTTONGAP*(outerRadius/innerRadius)), true)
+    ctx.arc(500, 500, outerRadius, radian2 + BUTTONGAP, radian1 - BUTTONGAP, false)
+    ctx.fill()
   }
 
 })
